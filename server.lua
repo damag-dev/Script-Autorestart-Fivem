c406a095b1b5137bb2fd87ed4f6b15fde1ffdc6c
G.Grzegorz <damag.dev@gmail.com>
2020-08-14 12:58:00 +0200

    Update version to 2.0
    + Added the ability to restart scripts once at the beginning of the script
    + List of scripts to restart in one place
    + README file edited
    + Added config

--- server.lua
+++ server.lua
@@ -3,16 +3,23 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ------------------------------------------------
 ---------Script made by damag#4013--------------
--------------------v 1.0------------------------
+-------------------v 2.0------------------------
 ------------------------------------------------
 
 ---Config----
-weabhooklink = "Your weabhook link"
-ReplyUserName = "Your Discord Bot Name"
-AvatarURL = "logo url"
+weabhooklink = "" ---weabhook link
+ReplyUserName = "Autorestart" ---Bot name
+AvatarURL = "" ---Url of avatar 
+local scripts = {'script1', 'script2'}  ---Scripts to restart
+local allert_msg = "'ALLERT' autorestart in 10 sec"  ---Allert before script restart message on discord
+local restart_title = "Autorestart" ---Script restart title on discord
+local restart_msg = "AUTO RESTART\nStatus - ``Successful``\nNext restart in - `6h`"  ---Script restart message on discord
+local everytime = true ---if false then will restart scripts only once at the beginning of the script
+local time_break = 21600000  ---time between restarts [6h] [Set everytime to true to use it]
 
-
---- dont edit below
+------------------------------------------------
+---------------DONT EDIT BELOW------------------
+------------------------------------------------
 function sendToDiscord(name, message, color)
     local connect = {
           {
@@ -26,38 +33,26 @@ function sendToDiscord(name, message, color)
       }
     PerformHttpRequest(weabhooklink, function(err, text, headers) end, 'POST', json.encode({username = ReplyUserName, embeds = connect, avatar_url = AvatarURL}), { ['Content-Type'] = 'application/json' })
   end
-  
 
---- First autorestart
-local app1 = "Name of your srcipt"
 
 Citizen.CreateThread(function()
   while true do 
     Citizen.Wait(1)
-    PerformHttpRequest(weabhooklink, function(err, text, headers) end, 'POST', json.encode({username = ReplyUserName, content = "ALLERT autorestart in 10 sec", avatar_url = AvatarURL}), { ['Content-Type'] = 'application/json' }) 
+    PerformHttpRequest(weabhooklink, function(err, text, headers) end, 'POST', json.encode({username = ReplyUserName, content = allert_msg, avatar_url = AvatarURL}), { ['Content-Type'] = 'application/json' }) 
       Citizen.Wait(10000)
-     StopResource(app1)
-     Citizen.Wait(100)
-     StartResource(app1)
-     sendToDiscord("Discord Control", "AUTO RESTART\nStatus - `Successful`\nNext restart in - `30 min`", 1645055)
-     Citizen.Wait(1800000) --- wiat 30 min
+      for index, value in next, scripts do
+    StopResource(value)
+    Citizen.Wait(1000)
+    StartResource(value)
+    end
+     sendToDiscord(restart_title, restart_msg, 1645055)
+     if everytime then
+        Citizen.Wait(time_break)
+     else
+      break 
+     end
   end
 end)
--- Next autorestart
--- If you want add next script to autorestart all you need to do is add new local with script name add new Citizen.CreateThread(function()
--- with you have bellow but with app2 insted app1 like this one bellow or look into the template in README
-local app2 = "Name of your srcipt"
-
-Citizen.CreateThread(function()
-  while true do 
-    Citizen.Wait(1)
-    PerformHttpRequest(weabhooklink, function(err, text, headers) end, 'POST', json.encode({username = ReplyUserName, content = "ALLERT autorestart in 10 sec", avatar_url = AvatarURL}), { ['Content-Type'] = 'application/json' }) 
-      Citizen.Wait(10000)
-     StopResource(app2)
-     Citizen.Wait(100)
-     StartResource(app2)
-     sendToDiscord("Discord Control", "AUTO RESTART\nStatus - `Successful`\nNext restart in - `30 min`", 1645055)
-     Citizen.Wait(1800000) --- wiat 30 min
-  end
-end)
-
+------------------------------------------------
+-----------YOUR OWN RESTARTS BELOW--------------
+------------------------------------------------
